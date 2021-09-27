a312b318d042a0bce6df428beabc130414ac2435
JeEonPark <ggungtoo@icloud.com>
2021-09-27 23:21:35 +0900

    2016 문제 커밋

---
+++ Programmers/Python/2016.py
@@ -0,0 +1,21 @@
+def solution(a, b):
+    
+    months = {}
+    
+    for i in range(1, 13):
+        if i <= 7:
+            if i == 2:
+                months[i] = 29
+            elif i % 2 == 1: #1,3,5,7월
+                months[i] = 31
+            else:
+                months[i]
+    
+    if a <= 7:
+        
+    
+    answer = ''
+    return answer
+
+
+solution(1,2)
