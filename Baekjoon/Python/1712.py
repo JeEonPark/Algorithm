ebfb9ab5f3dce296c5ef2a0ff0d23b22f00d83a8
JeEonPark <ggungtoo@icloud.com>
2021-11-07 02:02:29 +0900

    Commit

--- Baekjoon/Python/1712.py
+++ Baekjoon/Python/1712.py
@@ -2,7 +2,7 @@ def solution(n, lost, reserve):
     answer = 0
     
     for i in range(1,n+1): 
-        if i not in lost: #안잃어버린 새끼들
+        if i not in lost: #안잃어버린 애들
             answer += 1
         else:
             if i in reserve: #잃어버렸는데 리저브있는 애들
